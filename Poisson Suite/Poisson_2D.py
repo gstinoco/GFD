b98fbd7327fe4241b489aea596d294ba16043bff
gstinoco <gstinoco@gmail.com>
2022-10-30 21:29:32 -0600

    Py comments

--- Poisson Suite/Poisson_2D.py
+++ Poisson Suite/Poisson_2D.py
@@ -3,22 +3,16 @@ from sys import path
 path.insert(0, 'Generales/')
 import Gammas
 
-## Diferencias Finitas Generalizadas
-# Se resuelve el problema de Diferencias Finitas Generalizadas.
-def BuscarVecinos(p, tt, nvec):
-    m   = len(p[:,0])                                                              # Se encuentra el tamaño de la triangulación.
-    vec = np.zeros([m, nvec*2])                                                      # Se inicializa el arreglo para guardar los vecinos.
-
-    for i in np.arange(m):                                                         # Para cada uno de los nodos.
-        kn   = np.argwhere(tt == i+1)                                                # Se busca en qué triángulos aparece el nodo.
-        #print('Los triángulos donde aparece el nodo', i, ' son:', kn[:,0])
-        vec2 = np.setdiff1d(tt[kn[:,0]], i+1)                                        # Se guardan los vecinos dentro de vec2.
-        #print('Los vecinos del nodo', i+1, ' son:', vec2)
-        vec2 = np.vstack([vec2])                                                     # Se convierte vec2 en una columna.
-        nvec = sum(vec2[0,:] != 0)                                                   # Se calcula el número de vecinos del nodo.
-        for j in np.arange(nvec):                                                    # Para cada uno de los vecinos.
-            vec[i,j] = vec2[0,j]                                                       # Se guardan los vecinos.
-
+def BuscarVecinos(p, tt, nvec):                                                     # Script to find the neighbors in a triangulation.
+    m   = len(p[:,0])                                                               # The size if the triangulation is obtained.
+    vec = np.zeros([m, nvec*2])                                                     # The array for the neighbors is initialized.
+    for i in np.arange(m):                                                          # For each of the nodes.
+        kn   = np.argwhere(tt == i+1)                                               # Se busca en qué triángulos aparece el nodo.
+        vec2 = np.setdiff1d(tt[kn[:,0]], i+1)                                       # Se guardan los vecinos dentro de vec2.
+        vec2 = np.vstack([vec2])                                                    # Se convierte vec2 en una columna.
+        nvec = sum(vec2[0,:] != 0)                                                  # Se calcula el número de vecinos del nodo.
+        for j in np.arange(nvec):                                                   # Para cada uno de los vecinos.
+            vec[i,j] = vec2[0,j]                                                    # Se guardan los vecinos.
     return vec
 
 def Poisson_Mesh(x, y, phi, f):
@@ -30,7 +24,6 @@ def Poisson_Mesh(x, y, phi, f):
     tol      = np.finfo(float).eps                                                  # La tolerancia sera eps.
     phi_ap   = np.zeros([m,n])                                                      # Se inicializa phi_ap con ceros.
     phi_ex   = np.zeros([m,n])                                                      # Se inicializa phi_ex con ceros.
-
     ## Se fijan las condiciones de Frontera
     for i in range(m):                                                              # Para cada uno de los nodos en las fronteras en x.
         phi_ap[i, 0]   = phi(x[i, 0],   y[i, 0])                                    # Se asigna la condición de frontera en el primer y.
@@ -38,11 +31,9 @@ def Poisson_Mesh(x, y, phi, f):
     for j in range(n):                                                              # Para cada uno de los nodos en las fronteras en y.
         phi_ap[0,   j] = phi(x[0,   j], y[0,   j])                                  # Se asigna la condición de frontera en el primer x.
         phi_ap[m-1, j] = phi(x[m-1, j], y[m-1, j])                                  # Se asigna la condición de frontera en el último x.
-  
     ## Se calculan los valores de los Gamma
     L = np.vstack([[0], [0], [2], [0], [2]])                                        # Se asignan los valores del operador dierencial.
-    Gamma = Gammas.Mesh(x, y, L)                                                         # Se calculan las Gammas.
-
+    Gamma = Gammas.Mesh(x, y, L)                                                    # Se calculan las Gammas.
     ## Se hace el Método de Diferencias Finitas Generalizadas
     while err >= tol:                                                               # Mientras que el error sea mayor que la tolerancia.
         err = 0                                                                     # Error se hace cero para poder actualizar.
@@ -57,15 +48,12 @@ def Poisson_Mesh(x, y, phi, f):
                     Gamma[i, j, 6]*phi_ap[i - 1, j - 1] + \
                     Gamma[i, j, 7]*phi_ap[i    , j - 1] + \
                     Gamma[i, j, 8]*phi_ap[i + 1, j - 1]))/Gamma[i, j, 0]            # Se calcula phi_ap en el nodo central.
-        
                 err = max(err, abs(t - phi_ap[i, j]));                              # Se calcula el error.
                 phi_ap[i,j] = t;                                                    # Se asigna el valor calculado previamente.
-
     ## Se guarda la solución exacta
     for i in range(m):                                                              # Para todos los nodos en x.
         for j in range(n):                                                          # Para todos los nodos en y.
             phi_ex[i,j] = phi(x[i,j], y[i,j])                                       # Se coloca la solución exacta.
-
     return phi_ap, phi_ex
 
 def Poisson_Tri(p, tt, pb, phi, f):
@@ -76,18 +64,14 @@ def Poisson_Tri(p, tt, pb, phi, f):
     tol      = np.finfo(float).eps                                                  # La tolerancia será eps.
     phi_ap   = np.zeros([m])                                                        # Se inicializa phi_ap con ceros.
     phi_ex   = np.zeros([m])                                                        # Se inicializa phi_ex con ceros.
-
     ## Conduciones de Frontera
     for i in np.arange(mf):                                                         # Para cada uno de los nodos de frontera.
         phi_ap[i]   = phi(pb[i, 0],   pb[i, 1])                                     # Se agrega la condición de frontera.
-
     ## Primero se calculan los nodos vecinos
     vec = BuscarVecinos(p, tt, nvec)                                                # Se hace encuentran los vecinos.
-
     ## Ahora, se calculan las Gammas
     L = np.vstack([[0], [0], [2], [0], [2]])                                        # Se asignan los valores para el operador diferencial.
-    Gamma = Gammas.Cloud(p, pb, vec, L)                                                   # Se calculan las Gammas.
-
+    Gamma = Gammas.Cloud(p, pb, vec, L)                                             # Se calculan las Gammas.
     while err >= tol:                                                               # Mientras que el error sea mayor que la tolerancia.
         err = 0                                                                     # Error se hace cero para poder actualizar.
         for i in np.arange(mf, m):                                                  # Para cada uno de los nodos interiores.
@@ -95,14 +79,11 @@ def Poisson_Tri(p, tt, pb, phi, f):
             nvec = sum(vec[i,:] != 0)                                               # Se calcula el número de vecinos que tiene el nodo.
             for j in np.arange(1,nvec+1):                                           # Para cada uno de los vecinos.
                 phitemp = phitemp + Gamma[i, j]*phi_ap[int(vec[i, j-1])-1]          # Se calcula hacen diferencias finitas.
-
             t = (f(p[i, 0], p[i, 1]) - phitemp)/Gamma[i,0]                          # Se calcula el valor de phi_ap en el nodo central.
             err = max(err, abs(t - phi_ap[i]));                                     # Se calcula el error.
             phi_ap[i] = t;                                                          # Se asigna el valor calculado previamente.
-
         for i in range(m):                                                          # Para cada uno de los nodos.
             phi_ex[i] = phi(p[i,0], p[i,1])                                         # Se coloca la solución exacta.
-
     return phi_ap, phi_ex, vec
 
 def Poisson_Cloud(p, pb, vec, phi, f):
@@ -113,14 +94,11 @@ def Poisson_Cloud(p, pb, vec, phi, f):
     tol      = np.finfo(float).eps                                                  # La tolerancia será eps.
     phi_ap   = np.zeros([m])                                                        # Se inicializa phi_ap con ceros.
     phi_ex   = np.zeros([m])                                                        # Se inicializa phi_ex con ceros.
-
     for i in np.arange(mf):                                                         # Para cada uno de los nodos de frontera.
         phi_ap[i]   = phi(pb[i, 0],   pb[i, 1])                                     # Se agrega la condición de frontera.
-
     # Ahora, se calculan las Gammas
     L = np.vstack([[0], [0], [2], [0], [2]])                                        # Se asignan los valores para el operador diferencial.
-    Gamma = Gammas.Cloud(p, pb, vec, L)                                                   # Se calculan las Gammas.
-
+    Gamma = Gammas.Cloud(p, pb, vec, L)                                             # Se calculan las Gammas.
     while err >= tol:                                                               # Mientras que el error sea mayor que la tolerancia.
         err = 0                                                                     # Error se hace cero para poder actualizar.
         for i in np.arange(mf, m):                                                  # Para cada uno de los nodos interiores.
@@ -128,12 +106,9 @@ def Poisson_Cloud(p, pb, vec, phi, f):
             nvec = sum(vec[i,:] != 0)                                               # Se calcula el número de vecinos que tiene el nodo.
             for j in np.arange(1,nvec+1):                                           # Para cada uno de los vecinos.
                 phitemp = phitemp + Gamma[i, j]*phi_ap[int(vec[i, j-1])-1]          # Se calcula hacen diferencias finitas.
-
             t = (f(p[i, 0], p[i, 1]) - phitemp)/Gamma[i,0]                          # Se calcula el valor de phi_ap en el nodo central.
             err = max(err, abs(t - phi_ap[i]));                                     # Se calcula el error.
             phi_ap[i] = t;                                                          # Se asigna el valor calculado previamente.
-
         for i in range(m):                                                          # Para cada uno de los nodos.
             phi_ex[i] = phi(p[i,0], p[i,1])                                         # Se coloca la solución exacta.
-
     return phi_ap, phi_ex
--- Poisson Suite/run.py
+++ Poisson Suite/run.py
@@ -4,7 +4,6 @@ from sys import path
 path.insert(0, 'Generales/')
 import Errores
 import Graficar
-path.insert(0, 'Poisson/')
 import Poisson_2D
 
 ## Se cargan los datos de la region
@@ -46,15 +45,15 @@ def f(x,y):
 
 phi_ap, phi_ex = Poisson_2D.Poisson_Cloud(p, pb, vec, phi, f)
 er = Errores.Cloud_Static(p, vec, phi_ap, phi_ex)
-print('El error cometido para el método es de: ', er)
+print('El error cometido para la nube', nube, ' es de: ', er)
 Graficar.Cloud_Static(p, phi_ap, phi_ex)
 
 phi_ap, phi_ex, vec = Poisson_2D.Poisson_Tri(p, tt, pb, phi, f)
 er = Errores.Cloud_Static(p, vec, phi_ap, phi_ex)
-print('El error cometido para el método es de: ', er)
+print('El error cometido para la triangulación', nube,  ' es de: ', er)
 Graficar.Cloud_Static(p, phi_ap, phi_ex)
 
 phi_ap, phi_ex = Poisson_2D.Poisson_Mesh(x, y, phi, f)
 er = Errores.Mesh_Static(x, y, phi_ap, phi_ex)
-print('El error cometido para el método es de: ', er)
+print('El error cometido para la malla', malla, 'es de: ', er)
 Graficar.Mesh_Static(x, y, phi_ap, phi_ex)
